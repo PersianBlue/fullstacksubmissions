3569a94f198b73d497d1cc588be9615c33b49053
PersianBlue <71480723+PersianBlue@users.noreply.github.com>
2023-08-06 10:56:26 -0500

    exercise 4.2 completed

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const config = require("./utils/config");
+const app = express();
+const cors = require("cors");
+const BlogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+
+app.use(cors());
+// app.use(express.static("build"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use("/api/blogs", BlogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
+
+/*
+
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/notes", notesRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
+
+*/
